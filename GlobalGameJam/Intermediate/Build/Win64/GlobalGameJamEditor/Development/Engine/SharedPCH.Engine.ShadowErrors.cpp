cbfd60519fb73e60dc994c7cbc2fba4a3de2f48c
Rickors <rickors4@gmail.com>
2021-01-29 13:28:04 +0100

    Carpeta bien hecha

--- GlobalGameJam/Intermediate/Build/Win64/GlobalGameJamEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29336
-#include "C:/Users/Javi/Documents/GitHub/GlobalGameJam2021/GlobalGameJam/Intermediate/Build/Win64/GlobalGameJamEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
