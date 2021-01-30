b3075f958f086b685c8bfeefe3f10c245ced153d
Rickors <rickors4@gmail.com>
2021-01-30 17:08:12 +0100

    Camara del personaje

--- Source/GlobalGameJam/GlobalGameJamCharacter.cpp
+++ Source/GlobalGameJam/GlobalGameJamCharacter.cpp
@@ -57,25 +57,10 @@ void AGlobalGameJamCharacter::SetupPlayerInputComponent(class UInputComponent* P
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	//PlayerInputComponent->BindAxis("MoveForward", this, &AGlobalGameJamCharacter::MoveForward);
-	//PlayerInputComponent->BindAxis("MoveRight", this, &AGlobalGameJamCharacter::MoveRight);
 
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("Turn", this, &AGlobalGameJamCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &AGlobalGameJamCharacter::LookUpAtRate);
 
-
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &AGlobalGameJamCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &AGlobalGameJamCharacter::LookUpAtRate);
-
-	// handle touch devices
-	//PlayerInputComponent->BindTouch(IE_Pressed, this, &AGlobalGameJamCharacter::TouchStarted);
-	//PlayerInputComponent->BindTouch(IE_Released, this, &AGlobalGameJamCharacter::TouchStopped);
-
-	// VR headset functionality
-	//PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AGlobalGameJamCharacter::OnResetVR);
 }
 
 
@@ -102,53 +87,30 @@ void AGlobalGameJamCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVecto
 
 void AGlobalGameJamCharacter::TurnAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+	float CameraRot = InitialYaw + Rate;
+
+	if (CameraRot<Grades && CameraRot >-Grades && Rate != 0.f && Controller && Controller->IsLocalPlayerController())
+	{
+		APlayerController* const PC = CastChecked<APlayerController>(Controller);
+		PC->AddYawInput(Rate);
+		InitialYaw += Rate;
+	}
 
 	FVector Forward = FollowCamera->GetForwardVector();
 	FRotator ResultRotated = Forward.Rotation();
 	SetActorRotation(ResultRotated);
+
 
 }
 
 void AGlobalGameJamCharacter::LookUpAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
-
-	FVector Forward = FollowCamera->GetForwardVector();
-	FRotator ResultRotated = Forward.Rotation();
-	SetActorRotation(ResultRotated);
-}
-
-/*
-void AGlobalGameJamCharacter::MoveForward(float Value)
-{
-	if ((Controller != nullptr) && (Value != 0.0f))
+	float CameraRot = InitialPitch + Rate;
+	if (CameraRot<Grades && CameraRot >-Grades && Rate != 0.f && Controller && Controller->IsLocalPlayerController())
 	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get forward vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, Value);
+		APlayerController* const PC = CastChecked<APlayerController>(Controller);
+		PC->AddPitchInput(Rate);
+		InitialPitch += Rate;
 	}
+
 }
-
-void AGlobalGameJamCharacter::MoveRight(float Value)
-{
-	if ( (Controller != nullptr) && (Value != 0.0f) )
-	{
-		// find out which way is right
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get right vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		// add movement in that direction
-		AddMovementInput(Direction, Value);
-	}
-}
-
-*/
